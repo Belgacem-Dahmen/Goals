c5b3345d673efcea96def1f9359f5e89534d599e
Belgacem-Dahmen <belgacem.dahmen@gmail.com>
2022-09-08 03:31:11 +0200

    depot2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("test")
+const express = require('express');
+const dotenv = require('dotenv');
+const port = process.env.DEV_PORT || 5000
+
+const app= express();
+app.listen(port, () => console.log(`Server is listening to port ${port}` ))
+
+
